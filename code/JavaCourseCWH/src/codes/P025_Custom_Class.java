ac2db41180ad309a28d97d16f01b0b2f1ae65715
Krish Halder <geonextgis@gmail.com>
2024-12-24 04:58:13 +0530

    class added.

--- code/JavaCourseCWH/src/codes/P025_Custom_Class.java
+++ code/JavaCourseCWH/src/codes/P025_Custom_Class.java
@@ -1,42 +1,52 @@
 package code.JavaCourseCWH.src.codes;
-
+// Defining the Employee class
 class Employee {
-    int id;
-    String name;
-    int salary;
+    // Fields (attributes) of the Employee
+    int id; // Employee ID
+    String name; // Employee name
+    int salary; // Employee salary
 
+    // Method to print employee details
     public void print_details() {
+        // Printing the Employee's ID and Name
         System.out.println("Employee ID: " + id);
         System.out.println("Employee Name: " + name);
     }
 
+    // Method to return the Employee's salary
     public int getSalary() {
         return salary;
     }
 }
 
+// Main class to demonstrate the Employee class
 public class P025_Custom_Class {
     public static void main(String[] args) {
+        // Printing a message to indicate program start
         System.out.println("This is our custom class");
 
-        Employee krish = new Employee();
-        Employee abir = new Employee();
+        // Creating objects of the Employee class
+        Employee krish = new Employee(); // First employee object
+        Employee abir = new Employee();  // Second employee object
 
-        // Setting attributes/properties
-        krish.id = 1;
-        krish.name = "Krishnagopal Halder";
-        krish.salary = 30000;
+        // Setting attributes/properties for the first employee (Krish)
+        krish.id = 1; // Setting ID for Krish
+        krish.name = "Krishnagopal Halder"; // Setting name for Krish
+        krish.salary = 30000; // Setting salary for Krish
 
-        abir.id = 2;
-        abir.name = "Abir Ghosh";
-        abir.salary = 25000;
+        // Setting attributes/properties for the second employee (Abir)
+        abir.id = 2; // Setting ID for Abir
+        abir.name = "Abir Ghosh"; // Setting name for Abir
+        abir.salary = 25000; // Setting salary for Abir
         
-        // Printing the attributes/properties
-        // System.out.println("The ID of the employee: " + krish.id);
-        // System.out.println("The name of the employee: " + krish.name);
-        krish.print_details();
+        // Printing the details of the first employee (Krish)
+        krish.print_details(); // Calling the method to print details
+        // Printing the salary of Krish
         System.out.println("Salary of Krish: " + krish.getSalary());
-        abir.print_details();
+
+        // Printing the details of the second employee (Abir)
+        abir.print_details(); // Calling the method to print details
+        // Printing the salary of Abir
         System.out.println("Salary of Abir: " + abir.getSalary());
     }
 }
